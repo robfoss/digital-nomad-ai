89e2fd4f2f5abfe64700422e6b79b1515d5a4924
robfoss <robfoss@rocketmail.com>
2023-04-15 12:27:33 -0400

    added DALLE AI image generator

--- src/app/[query]/page.tsx
+++ src/app/[query]/page.tsx
@@ -7,9 +7,10 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-
-export default async function SuggestionPage(props: {params : {query: string}}) {
-    const pathNameParams = new URLSearchParams(
+export default async function RequestPage(props: {
+  params: { query: string };
+}) {
+  const pathNameParams = new URLSearchParams(
     decodeURIComponent(props.params.query)
   );
   const { location, month, budget, activity } = Object.fromEntries(
@@ -23,14 +24,15 @@ export default async function SuggestionPage(props: {params : {query: string}}) 
   if (budget) textPrompt += ` with budget of ${budget}$ per month`;
   textPrompt += " and explain why. In format Location - Description";
 
-    const response = await openai.createChatCompletion({
+  const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: textPrompt }],
     temperature: 0,
     max_tokens: 2000,
   });
-    console.log(response.data.choices[0].message.content);
-    const [, ...entries] = JSON.stringify(
+
+
+  const [, ...entries] = JSON.stringify(
     response.data.choices[0].message.content
   ).split("\\n\\n");
 
@@ -48,9 +50,22 @@ export default async function SuggestionPage(props: {params : {query: string}}) 
 
     destinations.push({ location, description, img: image.data.data[0].url });
   }
-  console.log(destinations);
-    
-    return <div>
-        <h1>My Suggestion</h1>
+
+  return (
+      <div className="flex flex-col gap-4">
+      {destinations.map(({ location, description, img }) => (
+        <div key={location} className="card lg:card-side bg-base-100 shadow-xl">
+
+          <figure className="relative w-full min-w-[230px] max-w-[230px]">
+            <Image src={img} alt={location} fill />
+          </figure>
+
+          <div className="card-body">
+            <h2 className="card-title">{location}</h2>
+            <p>{description}</p>
+          </div>
+        </div>
+      ))}
     </div>
+  );
 }
